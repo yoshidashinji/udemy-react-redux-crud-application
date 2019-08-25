edf8bfc7f3ff19bf110d94301f4f9a90c4489b8f
吉田真路 <yoshidashinji@yoshidamakotojinoMacBook-Pro.local>
2019-08-25 21:41:37 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>;
+    // const greeting = "Hi!, Tom"
+    // const dom = <h1>{greeting}</h1>;
+    // return dom;
+
+    // return <h1>Hello, world!</h1>;
+
+    // return <input type="text" onChange={() => {console.log("I am changed!")}} />;
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am changed!")}} />
+      </React.Fragment>
+    )
   }
 }
 export default App;
