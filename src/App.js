a6c087c5a36f232bcf093a4710421ddc731b3be5
吉田真路 <yoshidashinji@yoshidamakotojinoMacBook-Pro.local>
2019-08-27 19:28:59 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,50 +1,27 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-// class App extends Component {
-//   render() {
-//     // const greeting = "Hi!, Tom"
-//     // const dom = <h1>{greeting}</h1>;
-//     // return dom;
+const App = () => (<Counter></Counter>)
 
-//     // return <h1>Hello, world!</h1>;
-
-//     // return <input type="text" onChange={() => {console.log("I am changed!")}} />;
-//     return(
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am changed!")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName", age: 1 }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  );
-}
-
-const User = (props) => {
-  return <div>Hi!, I am {props.name}, and {props.age} years old!</div>;
-}
-// User.defaultProps = {
-//   age: 1
-// }
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {count: 0}
+  }
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
